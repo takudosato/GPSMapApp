607ce9ea0b533293dddc420bd25f55e58a94253e
takudosato <takudo_sato@sobal.co.jp>
2020-07-03 14:31:17 +0900

    CommitMessage1

--- app/src/main/java/com/example/gpsmapapp/MainActivity.kt
+++ app/src/main/java/com/example/gpsmapapp/MainActivity.kt
@@ -9,6 +9,14 @@ import android.widget.EditText
 import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+
+    //緯度フィールド
+    private var _latitude = 0.0
+    //経度フィールド
+    private var _longitude = 0.0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -32,10 +40,13 @@ class MainActivity : AppCompatActivity() {
 
     fun onMapShowCurrentButtonClick(view: View) {
 
-        //入力欄に入力されたキーワード文字列を取得
-        val etSearchWord = findViewById<EditText>(R.id.etSearchWord)
-        var searchWord = etSearchWord.text.toString()
+        //フィールドの緯度と経度の値を元にマップアプリと連携するURI文字列を生成
+        val uriStr = "geo:${_latitude},${_longitude}"
+        //URI文字列からURIオブジェクトを生成
+        val uri = Uri.parse(uriStr)
+        //Intentオブジェクトを生成
+        val intent = Intent(Intent.ACTION_VIEW, uri)
 
-
+        startActivity(intent)
     }
 }
