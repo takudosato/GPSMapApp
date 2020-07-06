8a7920eeac592e9202ed7595a813634f0f10453a
takudosato <takudo_sato@sobal.co.jp>
2020-07-06 11:22:30 +0900

    GPS機能追加

--- app/src/main/java/com/example/gpsmapapp/MainActivity.kt
+++ app/src/main/java/com/example/gpsmapapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.gpsmapapp
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
 import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
@@ -18,8 +26,63 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //LocationManagerオブジェクトを取得
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        //位置情報が更新された際のリスナオブジェクトを生成
+        val locationLister = GPSLocationListnerer()
+        //位置情報の追跡を開始
+        //ACCESS_FINE_LOCATIONの許可がおりていないならば、
+        if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //許可を求めるダイアログを表示する
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+            ActivityCompat.requestPermissions(this@MainActivity, permissions, 1000)
+
+            //OnCreateを終了
+            return
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationLister)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        //ACCESS_FINE_LOCATIONに対するパーミッションダイアログで許可を選択したばあい
+        if(requestCode == 1000 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            //LocationManagerオブジェクトを取得
+            val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            //位置情報が更新された際のリスナオブジェクトを生成
+            val locationLister = GPSLocationListnerer()
+            //再度アクセスチェック
+            if(ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0f, locationLister)
+        }
+
+    }
+
+    private inner class GPSLocationListnerer : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            //locationから緯度・緯度を取得
+            _latitude = location.latitude
+            _longitude = location.longitude
+
+            //取得した緯度・経度を表示
+            val tvLatitude = findViewById<TextView>(R.id.tvLatitude)
+            tvLatitude.text = _latitude.toString()
+            val tvLongitude = findViewById<TextView>(R.id.tvLongitude)
+            tvLongitude.text = _longitude.toString()
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 
     fun onMapSearchButtonClick(view: View) {
